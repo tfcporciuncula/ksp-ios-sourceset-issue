8bf64c217870709e8831df748d2095964671cb07
Fred Porciúncula <tfcporciuncula@gmail.com>
2022-12-06 16:21:02 +0100

    Integrate on the iOS side

---
+++ shared/src/iosMain/kotlin/com/fredporciuncula/ksp/CommonComponentProvider.kt
@@ -0,0 +1,8 @@
+package com.fredporciuncula.ksp
+
+import com.fredporciuncula.CommonComponent
+import com.fredporciuncula.create
+
+object CommonComponentProvider {
+  val component = CommonComponent::class.create()
+}
