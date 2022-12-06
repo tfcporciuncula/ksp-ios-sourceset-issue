1dfad44cd2772469d0e907dc0992fdc7afc88379
Fred Porciúncula <tfcporciuncula@gmail.com>
2022-12-06 16:22:08 +0100

    Add comment about the issue

--- shared/src/iosMain/kotlin/com/fredporciuncula/ksp/CommonComponentProvider.kt
+++ shared/src/iosMain/kotlin/com/fredporciuncula/ksp/CommonComponentProvider.kt
@@ -1,7 +1,7 @@
 package com.fredporciuncula.ksp
 
 import com.fredporciuncula.CommonComponent
-import com.fredporciuncula.create
+import com.fredporciuncula.create // IDE doesn't understand this exists, even though it compiles
 
 object CommonComponentProvider {
   val component = CommonComponent::class.create()
